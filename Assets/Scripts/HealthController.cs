54cf5a3ab738a8f405ada5d08f12d676565e1398
Samuel Palacio <sampalaciomesa@gmail.com>
2024-05-19 14:50:04 -0500

    Organize player prefab

--- Assets/Scripts/HealthController.cs
+++ Assets/Scripts/HealthController.cs
@@ -13,7 +13,6 @@ public class HealthController : MonoBehaviour
         get => health;
         set
         {
-
             health = value;
             healthView.SetHealth(health, totalHealth);
 
